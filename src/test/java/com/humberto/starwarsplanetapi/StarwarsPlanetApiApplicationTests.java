ab769048767536bf032b4e62e121b7df58890cd0
humberto-ss <humberto_ss@hotmail.com>
2024-03-30 21:57:46 +0100

    project structure

---
+++ src/test/java/com/humberto/starwarsplanetapi/StarwarsPlanetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humberto.starwarsplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsPlanetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
