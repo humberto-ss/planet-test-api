fc740e3b024dd4ebfd4c5e69bf37cb13bd0a6f46
humberto-ss <humberto_ss@hotmail.com>
2024-03-31 15:26:17 +0200

    refactor package name and change DB proprieties on application.properties

--- src/main/java/com/humberto/planetapi/Application.java
+++ src/main/java/com/humberto/planetapi/Application.java
@@ -1,4 +1,4 @@
-package com.humberto.starwarsplanetapi;
+package com.humberto.planetapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
