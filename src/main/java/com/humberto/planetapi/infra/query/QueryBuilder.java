c2e5a085ee035daec4a0eaed2b05dd11afc0e4f8
humberto-ss <humberto_ss@hotmail.com>
2024-03-31 21:12:02 +0200

    Create query Builder to create queries dynamically

---
+++ src/main/java/com/humberto/planetapi/infra/query/QueryBuilder.java
@@ -0,0 +1,13 @@
+package com.humberto.planetapi.infra.query;
+
+import com.humberto.planetapi.domain.Planet;
+import com.humberto.planetapi.domain.PlanetDTO;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+
+public class QueryBuilder {
+    public static Example<Planet> makeQuery(Planet planet){
+        ExampleMatcher exampleMatcher = ExampleMatcher.matchingAll().withIgnoreCase().withIgnoreNullValues();
+        return Example.of(planet, exampleMatcher);
+    }
+}
